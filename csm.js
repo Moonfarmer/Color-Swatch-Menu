eae7d8a5aa23b41ed5bfacdf29a9679b82aa5f50
walshyb <bmwalshy@gmail.com>
2017-04-15 02:41:05 -0400

    add slight animation to swatches to further extend them on click

--- csm.js
+++ csm.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   $('#menu-toggle').on('click', function() {
     $('.swatch-container').toggleClass('hide').toggleClass('show');
+    $('.swatch').toggleClass('not').toggleClass('okay');
   });
 });
 
